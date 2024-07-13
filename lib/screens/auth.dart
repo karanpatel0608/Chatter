455b4b534f9b6e5c98e546437bd2afd77deb8fca
karanpatel0608 <202152315@iiitvadodara.ac.in>
2024-07-13 16:52:03 +0530

    Profile Picture and Username

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:chat_app/widgets/user_image_picker.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
@@ -19,30 +23,68 @@ class _AuthScreenState extends State<AuthScreen> {
   var _isLogin = true;
   var _userEmail = '';
   var _userPassword = '';
+  var _userName = '';
+  File? _selectedImage;
+  var _isAuthenticating = false;
 
   _submit() async {
     final isValid = _form.currentState!.validate();
 
-    if (!isValid) {
+    if (!isValid || (!_isLogin && _selectedImage == null)) {
+      // Show an error message
+      ScaffoldMessenger.of(context).clearSnackBars();
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Please complete the form correctly')),
+      );
       return;
     }
+
     _form.currentState!.save();
 
     try {
+      setState(() {
+        _isAuthenticating = true;
+      });
       if (_isLogin) {
         // Log user in
         final userCredentials = await _firebase.signInWithEmailAndPassword(
             email: _userEmail, password: _userPassword);
-        print(userCredentials);
+        // print(userCredentials);
       } else {
         final userCredentials = await _firebase.createUserWithEmailAndPassword(
             email: _userEmail, password: _userPassword);
-        print(userCredentials);
+        // print(userCredentials);
+
+        final storageRef = FirebaseStorage.instance
+            .ref()
+            .child('user_images')
+            .child('${userCredentials.user!.uid}.jpg');
+
+        await storageRef.putFile(_selectedImage!);
+        final imageUrl = await storageRef.getDownloadURL();
+        // print(imageUrl);
+
+        await FirebaseFirestore.instance
+            .collection('users')
+            .doc(userCredentials.user!.uid)
+            .set({
+          'username': _userName,
+          'email': _userEmail,
+          'image_url': imageUrl,
+        });
       }
     } on FirebaseAuthException catch (error) {
       ScaffoldMessenger.of(context).clearSnackBars();
       ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(content: Text(error.message ?? 'Authentication Failed')));
+    } catch (error) {
+      ScaffoldMessenger.of(context).clearSnackBars();
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content:  Text('An error occurred. Please try again later.')));
+
+      setState(() {
+        _isAuthenticating = false;
+      });
     }
   }
 
@@ -54,18 +96,23 @@ class _AuthScreenState extends State<AuthScreen> {
         return;
       }
 
-      final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+      final GoogleSignInAuthentication googleAuth =
+          await googleUser.authentication;
       final AuthCredential credential = GoogleAuthProvider.credential(
         accessToken: googleAuth.accessToken,
         idToken: googleAuth.idToken,
       );
 
       final userCredential = await _firebase.signInWithCredential(credential);
-      print(userCredential);
+      // print(userCredential);
     } on FirebaseAuthException catch (error) {
       ScaffoldMessenger.of(context).clearSnackBars();
       ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(content: Text(error.message ?? 'Google Sign-In Failed')));
+    } catch (error) {
+      ScaffoldMessenger.of(context).clearSnackBars();
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text('An error occurred. Please try again later.')));
     }
   }
 
@@ -99,7 +146,11 @@ class _AuthScreenState extends State<AuthScreen> {
                         mainAxisSize: MainAxisSize.min,
                         children: [
                           if (!_isLogin)
-                           UserImagePicker(),
+                            UserImagePicker(
+                              onPickImage: (pickedImage) {
+                                _selectedImage = pickedImage;
+                              },
+                            ),
                           TextFormField(
                             decoration: const InputDecoration(
                                 labelText: 'Email Address'),
@@ -118,6 +169,21 @@ class _AuthScreenState extends State<AuthScreen> {
                               _userEmail = value!;
                             },
                           ),
+                          if (!_isLogin)
+                          TextFormField(
+                            decoration:  const InputDecoration(labelText: 'Username'),
+                            enableSuggestions: false,
+                            validator: (value) {
+                              if(value == null || value.trim().isEmpty || value.trim().length < 4) {
+                                return 'Please enter atleast 4 characters.';
+                              }
+                              return null;
+                            },
+                            onSaved: (value) {
+                              _userName = value!;
+                            },
+                          ),
+
                           TextFormField(
                             decoration:
                                 const InputDecoration(labelText: 'Password'),
@@ -133,15 +199,18 @@ class _AuthScreenState extends State<AuthScreen> {
                             },
                           ),
                           const SizedBox(height: 12),
-                          ElevatedButton(
-                            onPressed: _submit,
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: Theme.of(context)
-                                  .colorScheme
-                                  .primaryContainer,
+                          if (_isAuthenticating)
+                            const CircularProgressIndicator(),
+                          if (!_isAuthenticating) // Add this line
+                            ElevatedButton(
+                              onPressed: _submit,
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Theme.of(context)
+                                    .colorScheme
+                                    .primaryContainer,
+                              ),
+                              child: Text(_isLogin ? 'Login' : 'Signup'),
                             ),
-                            child: Text(_isLogin ? 'Login' : 'Signup'),
-                          ),
                           TextButton(
                             onPressed: () {
                               setState(() {
--- lib/widgets/user_image_picker.dart
+++ lib/widgets/user_image_picker.dart
@@ -3,7 +3,12 @@ import 'package:image_picker/image_picker.dart';
 import 'dart:io';
 
 class UserImagePicker extends StatefulWidget {
-  const UserImagePicker({super.key});
+  const UserImagePicker({
+    super.key,
+    required this.onPickImage,
+  });
+
+  final void Function(File pickedImage) onPickImage;
 
   @override
   State<StatefulWidget> createState() {
@@ -12,18 +17,23 @@ class UserImagePicker extends StatefulWidget {
 }
 
 class _UserImagePickerState extends State<UserImagePicker> {
-
   File? _pickedImageFile;
 
   void _pickImage() async {
-    final pickedImage = await ImagePicker().pickImage(source: ImageSource.camera, imageQuality: 50, maxWidth: 150,);
+    final pickedImage = await ImagePicker().pickImage(
+      source: ImageSource.camera,
+      imageQuality: 50,
+      maxWidth: 150,
+    );
 
-  if(pickedImage ==null){
-    return;
-  }
-    setState((){
+    if (pickedImage == null) {
+      return;
+    }
+    setState(() {
       _pickedImageFile = File(pickedImage.path);
     });
+
+    widget.onPickImage(_pickedImageFile!);
   }
 
   @override
@@ -33,11 +43,18 @@ class _UserImagePickerState extends State<UserImagePicker> {
         CircleAvatar(
           radius: 40,
           backgroundColor: Colors.grey,
-          foregroundImage: _pickedImageFile != null ? FileImage(_pickedImageFile!): null,
+          foregroundImage:
+              _pickedImageFile != null ? FileImage(_pickedImageFile!) : null,
         ),
-        TextButton.icon(onPressed: _pickImage , icon: const Icon(Icons.image) , label: Text('Add Image', style: TextStyle(
-          color: Theme.of(context).colorScheme.primary,
-        ),))
+        TextButton.icon(
+            onPressed: _pickImage,
+            icon: const Icon(Icons.image),
+            label: Text(
+              'Add Image',
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.primary,
+              ),
+            ))
       ],
     );
   }
